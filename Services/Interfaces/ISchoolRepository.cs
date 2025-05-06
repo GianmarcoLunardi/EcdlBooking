70f52bcb06372740d00b065d301817c945de0164
GianmarcoLunardi <gianmarcofi@gmail.com>
2025-05-06 09:38:34 +0200

    implementazione classe school

---
+++ Services/Interfaces/ISchoolRepository.cs
@@ -0,0 +1,12 @@
+﻿using EcdlBooking.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EcdlBooking.Services.Interfaces
+{
+    public interface ISchoolRepository : IGenericRepo<School>
+    {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+
+        void Update(School school);
+    }
+}
---
+++ Services/Repository/SchoolRepository.cs
@@ -0,0 +1,29 @@
+﻿using EcdlBooking.Data;
+using EcdlBooking.Models;
+using EcdlBooking.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EcdlBooking.Services.Repository
+{
+    public class SchoolRepository : GenericRep<School>, ISchoolRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public SchoolRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public IEnumerable<SelectListItem> GetCategoryListForDropDown()
+        {
+            return _db.Schools.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.id.ToString()
+            });
+        }
+        public void Update(School school)
+        {
+            _db.Schools.Update(school);
+        }
+    }
+
+}
